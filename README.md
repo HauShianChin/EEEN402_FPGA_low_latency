7476890d5bcb8d5e6d3d0d1c5c336fa15f6e4a52
HauShianChin <109963218+HauShianChin@users.noreply.github.com>
2022-07-31 16:43:13 +1200

    2 bit 2 to 1 mux

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # EEEN402_FPGA_low_latency
 VHDL projects
 
-Academic experience of VHDL. demonstrating my understanding of dataflow design, strucutral design, behavourial design and a combination of the previous 3. 30/7/22
+Academic experience of VHDL. demonstrating my understanding of dataflow design, strucutral design, behavourial design and a combination of the previous 3. 31/7/22
---
+++ project_2_dataflow_2_bit/project_2_dataflow_2_bit.srcs/sources_1/new/mux_dataflow_2bit.vhd
@@ -0,0 +1,60 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07/31/2022 04:09:31 PM
+-- Design Name: 
+-- Module Name: mux_dataflow_2bit - Behavioral
+-- Project Name: 
+-- Target Devices: basys-3
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity mux_dataflow_2bit is
+    Port ( x : in STD_LOGIC_VECTOR (1 downto 0);
+           y : in STD_LOGIC_VECTOR (1 downto 0);
+           s : in STD_LOGIC;
+           m : out STD_LOGIC_VECTOR (1 downto 0));
+end mux_dataflow_2bit;
+
+architecture Behavioral of mux_dataflow_2bit is
+
+signal sbar : STD_LOGIC; 
+signal x_int : STD_LOGIC_VECTOR (1 downto 0);
+signal y_int : STD_LOGIC_VECTOR (1 downto 0);
+
+begin
+sbar <= not s; 
+
+x_int (0) <= x (0) and s; 
+x_int (1) <= x (1) and sbar; 
+y_int (0) <= y (0) and s; 
+y_int (1) <= y (1) and sbar; 
+
+m (0) <= x_int (0) or y_int (0);
+
+m (1) <= x_int (1) or y_int (1);
+
+
+end Behavioral;
